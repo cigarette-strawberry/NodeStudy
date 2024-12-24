0617a2556ce083f7280f24052c9b51a4c2d6bde3
cigarette <2467337566@qq.com>
2024-12-24 23:27:44 +0800

    wip:Nodejs 第三十一章（响应头和请求头）

--- Response Request Header/index.js
+++ Response Request Header/index.js
@@ -11,11 +11,19 @@ app.use((req, res, next) => {
 
   // Access-Control-Allow-Methods 默认只支持 GET POST HEAD
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+
+  // Access-Control-Allow-Headers 支持 application/json
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.set('mini', 1234); // 自定义响应头
+  res.setHeader('Access-Control-Expose-Headers', 'mini'); // 后端必须抛出来
+  res.json({
+    code: 200,
+    message: 'Hello World!'
+  });
 });
 
 app.get('/get', (req, res) => {
@@ -25,11 +33,44 @@ app.get('/get', (req, res) => {
   });
 });
 
+app.post('/post', (req, res) => {
+  res.json({
+    code: 200,
+    message: 'post/Hello World!'
+  });
+});
+
 app.patch('/patch', (req, res) => {
   res.json({
     code: 200,
     message: 'patch/Hello World!'
   });
+});
+
+/**
+ *  预检请求 OPTIONS 这个请求是浏览器发起的
+ * 需要满足下面条件之一才会发起
+ * 1、Content-Type application/json
+ * 2、自定义请求头
+ * 3、非普通请求(get,post)   PATCH, PUT, DELETE
+ * */
+
+/**
+ *  sse
+ * webSocket 实时通讯 前端给后端发 后端也可以给前端发
+ * 全双工通讯
+ *
+ *
+ * 大屏项目 后端需要实时返回给前端 前端不需要传递信息
+ * 单工通讯 也就是说 只能后端给前端发
+ * */
+
+app.get('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  setInterval(() => {
+    res.write(`event: test\n`); // 默认是message
+    res.write(`data: ${new Date().toISOString()} - Hello SSE\n\n`);
+  }, 1000);
 });
 
 app.listen(3000, () => {
