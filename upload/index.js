72c37011e706bc0c98119669b78d7b1918a55c68
cigarette <2467337566@qq.com>
2025-03-29 21:57:11 +0800

    feat:Nodejs 第五十八章（大文件上传）

--- upload/index.js
+++ upload/index.js
@@ -7,7 +7,6 @@ import path from 'node:path';
 // 1、初始化
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // cb(null, path.join(process.cwd(), 'uploads/'));
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
@@ -15,17 +14,39 @@ const storage = multer.diskStorage({
   }
 });
 
-// const upload = multer({ storage });
-const upload = multer({ dest: 'uploads/' });
-const http = express();
+const upload = multer({ storage });
 
-http.use(cors());
-http.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-http.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), (req, res) => {
   res.send('ok');
 });
 
-http.listen(3000, () => {
+app.post('/merge', (req, res) => {
+  // 1、读取里面所有的切片
+  const uploadDir = path.join(process.cwd(), 'uploads');
+
+  const dirs = fs.readdirSync(uploadDir);
+
+  // 2、排序
+  dirs.sort((a, b) => a.split('-')[0] - b.split('-')[0]);
+
+  // 3、合并
+  const filePath = path.join(process.cwd(), 'files', req.body.filename);
+  dirs.forEach(dir => {
+    // 读取文件
+    const file = fs.readFileSync(path.join(uploadDir, dir));
+    // 合并文件
+    fs.appendFileSync(filePath, file);
+    // 删除文件
+    fs.unlinkSync(path.join(uploadDir, dir));
+  });
+
+  res.send('ok');
+});
+
+app.listen(3000, () => {
   console.log('listening on port 3000');
 });
